40ecf9ee47fb94a837ca51795122e012e4a7ee21
Daniel <daniel_ye.nz@hotmail.com>
2022-07-05 20:40:23 -0700

    navbar-update-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,32 +5,31 @@ import { Route, Switch } from 'react-router-dom';
 import useStyles from './styles';
 import { Actors, MovieInformation, Movies, NavBar, Profile } from './';
 
-
 const App = () => {
-    const classes = useStyles ();
+    const classes = useStyles();
 
     return (
-    <div className = {classes.root}>
-    <CssBaseline />
-    <NavBar />
-        <main classname = {classes.content}>
-        <div className = {classes.toolbar} />
-            <Switch>
-                <Route exact path = "/movie/:id">
-                    <MovieInformation />
-                </Route>
-                <Route exact path = "/actors/:id">
-                    <Actors />
-                </Route>
-                <Route exact path = "/">
-                    <Movies />
-                </Route>
-                <Route exact path = "/profile/:id">
-                    <Profile />
-                </Route>
-            </Switch>
-        </main>
-    </div>
+        <div className={classes.root}>
+            <CssBaseline />
+            <NavBar />
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Switch>
+                    <Route exact path="/movie/:id">
+                        <MovieInformation />
+                    </Route>
+                    <Route exact path="/actors/:id">
+                        <Actors />
+                    </Route>
+                    <Route exact path="/">
+                        <Movies />
+                    </Route>
+                    <Route exact path="/profile/:id">
+                        <Profile />
+                    </Route>
+                </Switch>
+            </main>
+        </div>
     )
 
 };
